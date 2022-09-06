9b37b080510178d666a8ae8588f5e7823bd631ec
salmancz <salman.jsdev@gmail.com>
2022-09-06 23:25:31 +0530

    modified:   package-lock.json 	modified:   package.json 	new file:   src/firebase-config.js

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDmLT01Hc7uLetoR-TJKeBrb5atm-CNP6c",
+  authDomain: "blog-react-1d4bc.firebaseapp.com",
+  projectId: "blog-react-1d4bc",
+  storageBucket: "blog-react-1d4bc.appspot.com",
+  messagingSenderId: "640449339893",
+  appId: "1:640449339893:web:af805e357f6342607b80bc",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
